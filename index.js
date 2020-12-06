24faf34cf2b9bd8309451fe8217584b4764c5d87
Michael Morelli <mmorelli@ogmath.com>
2020-12-06 11:16:39 -0600

    update

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        // Swap it in and reload the page to get the new version.
+        window.applicationCache.swapCache();
+        if (confirm('A new version of this site is available. Load it?')) {
+            window.location.reload();
+        }
+        } else {
+        // Manifest didn't changed. Nothing new to server.
+        }
+    }, false);
+    
+    }, false);
+
 // Variable Declarations
 let my_element = document.createElement('h1');
 let my_span = document.createElement('span');
