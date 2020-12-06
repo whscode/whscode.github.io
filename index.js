88862fec375b5988b468f9e72e6599f0eb499d35
root <root@ogmath.com>
2020-12-06 14:59:20 -0600

    update

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-// // Check if a new cache is available on page load.
-// window.addEventListener('load', function(e) {
-
-//     window.applicationCache.addEventListener('updateready', function(e) {
-//         if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-//         // Browser downloaded a new app cache.
-//         // Swap it in and reload the page to get the new version.
-//         window.applicationCache.swapCache();
-//         if (confirm('A new version of this site is available. Load it?')) {
-//             window.location.reload();
-//         }
-//         } else {
-//         // Manifest didn't changed. Nothing new to server.
-//         }
-//     }, false);
-    
-//     }, false);
+// This is our source code for our website
+// Authors: Morelli,Lalo,Mario,Casper
 
 // Variable Declarations
 let my_element = document.createElement('h1');
