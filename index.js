6f80d8753a90da8fac6c03341c78986ed4436954
Michael Morelli <mmorelli@ogmath.com>
2020-12-06 13:10:22 -0600

    update

--- index.js
+++ index.js
@@ -32,8 +32,6 @@ header2.innerText = "Welcome to the WHS Code Club Website.";
 header2.appendChild(header2_span);
 document.body.appendChild(header2);
 
-function logoImg()
-{
-    var logoImg = document.getElementById('x');
-    logoImg.getElementsByTagName("img")[0].src = "./images/WHSCODE.png";
-}
+var whsCodeLogo = document.createElement('img');
+img.src = './images/WHSCODE.png';
+document.getElementById('container').appendChild(img);
